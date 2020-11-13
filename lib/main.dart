1645a08a53727366cf357f33a96fa3b3d62af790
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2020-11-13 01:04:39 -0300

    01.05 - Adicionando imagem e botao de contatos

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,23 @@ class BytebankApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Dashboard'),
         ),
+        body: Column(
+          children: <Widget>[
+            // Image.network('https://cdn.pixabay.com/photo/2019/10/15/06/03/pinwheel-4550711_960_720.jpg'),
+            Image.asset('images/bytebank_logo.png'),
+            Container(
+              height: 120,
+              width: 100,
+              color: Colors.green,
+              child: Column(
+                children: <Widget>[
+                  Icon(Icons.people),
+                  Text('Contacts'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
