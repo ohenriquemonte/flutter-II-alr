e638ee0eff98502acbcd52c3f8b84432407cd472
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2020-11-20 23:18:58 -0300

    04.01 - Configurando o builder do ListView

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,25 +1,24 @@
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
+  final List<Contact> contacts = List();
+
   @override
   Widget build(BuildContext context) {
+    // contacts.add(Contact(0, 'Henrique', 1000));
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Alex',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text('1000', style: TextStyle(fontSize: 16.0)),
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contact contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -36,5 +35,25 @@ class ContactsList extends StatelessWidget {
         child: Icon(Icons.add),
       ),
     );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+        child: ListTile(
+      title: Text(
+        contact.name,
+        style: TextStyle(fontSize: 24.0),
+      ),
+      subtitle: Text(contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0)),
+    ));
   }
 }
