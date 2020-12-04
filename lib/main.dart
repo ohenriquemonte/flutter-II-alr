5fcf28b1025e165209348460fbbdb27d3f1aa2a0
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2020-12-03 22:41:09 -0300

    05.06 - Criando o DAO para o contato

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:bytebank/database/app_database.dart';
-import 'package:bytebank/screens/contact_form.dart';
-import 'package:bytebank/screens/contacts_list.dart';
+// import 'package:bytebank/database/app_database.dart';
+// import 'package:bytebank/screens/contact_form.dart';
+// import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
-import 'models/contact.dart';
+// import 'models/contact.dart';
 
 void main() {
   runApp(BytebankApp());
--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,4 +1,5 @@
-import 'package:bytebank/database/app_database.dart';
+// import 'package:bytebank/database/app_database.dart';
+import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,8 @@ class ContactsList extends StatefulWidget {
 }
 
 class _ContactsListState extends State<ContactsList> {
+  final ContactDao _dao = ContactDao();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +20,7 @@ class _ContactsListState extends State<ContactsList> {
         ),
         body: FutureBuilder<List<Contact>>(
           initialData: List(),
-          future: findAll(),
+          future: _dao.findAll(),
           builder: (context, snapshot) {
             switch (snapshot.connectionState) {
               case ConnectionState.none:
